fe634e28b4a7b7aac0859b5b7518a55abc1ee43b
rubayet2027 <u2204002@student.cuet.ac.bd>
2025-12-21 14:52:33 +0600

    Manage Interest of owner api

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ const corsOptions = {
     process.env.CLIENT_URL || 'http://localhost:5173',
     'http://localhost:5173',
     'http://localhost:5174',
+    'http://localhost:5175',
     'https://krishilink-56c13.web.app',
     'https://krishilink-56c13.firebaseapp.com',
     'https://krishilink.vercel.app',
